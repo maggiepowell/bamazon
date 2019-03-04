2b3d05b43452193ab2c15952518c779ef631a348
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-03-04 14:12:39 -0800

    view products function added

--- bamazonManager.js
+++ bamazonManager.js
@@ -39,15 +39,23 @@ function manager() {
                 chosenItem = results[i];
             }
             }
-        // * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
-            connection.query("SELECT item_id, product_name, price, stock_quantity FROM products",
-
-            )
+            // * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
+            if (chosenItem === results[0]){
+                connection.query("SELECT item_id, product_name, price, stock_quantity FROM products",
+                function (err, results) {
+                    for (var i = 0; i < results.length; i++) {
+                      console.log("Id: " + results[i].item_id +
+                      " || Product: " + results[i].product_name + 
+                      " || Price: " + results[i].price +
+                      " || Stock: " + results[i].stock_quantity);
+                    }
+                }
+                )}
         })
-}
 
 // * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
 
 // * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
 
 // * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+}
